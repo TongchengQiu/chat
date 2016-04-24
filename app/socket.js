48d44a0e3a46b5003dc80a516a288d8607a6be35
TongchengQiu <TongchengQiu@gmail.com>
2016-04-25 05:58:38 +0800

    c

--- app/socket.js
+++ app/socket.js
@@ -1,7 +1,7 @@
 var io = require('socket.io-client');
 
 export default function (receiveFoo) {
-  var socket = io.connect('http://localhost:8888');
+  var socket = io.connect('http://localhost:3000');
 
   socket.on('receive', function (obj) {
     receiveFoo(obj);
